4b4c8c656e1bb02b5a26594d644915d22896ee73
Niklas Heer <me@nheer.io>
2025-11-21 22:59:57 +0100

    tdx 0.2.4

--- Formula/tdx.rb
+++ Formula/tdx.rb
@@ -1,65 +1,45 @@
 class Tdx < Formula
   desc "Your todos, in markdown, at terminal speed"
   homepage "https://github.com/niklas-heer/tdx"
-  version "0.2.2"
+  version "0.2.4"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-darwin-arm64.zip"
-      sha256 "8b8b0ff6a5b8e8e1e9f0a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2"
+      url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-darwin-arm64"
+      sha256 "0d8cd73e1f14be154535a92dad5101927b5b3592accad51fccf5a332ddae0d58"
 
       def install
-        libexec.install "tdx-darwin-arm64" => "tdx"
-        libexec.install "yoga.wasm"
-        (bin/"tdx").write <<~EOS
-          #!/bin/bash
-          cd "#{libexec}" && exec ./tdx "$@"
-        EOS
+        bin.install "tdx-darwin-arm64" => "tdx"
       end
     end
 
     on_intel do
-      url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-darwin-x64.zip"
-      sha256 "61da7721736cc34b4e91be290a2c8d79b24785fa2e48e5e486e5390f23e60518"
+      url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-darwin-x64"
+      sha256 "d71e829fe42b1254d9f0e6e52fa396bc8fa3a03e249c96a00e9d2b6d4188b5b2"
 
       def install
-        libexec.install "tdx-darwin-x64" => "tdx"
-        libexec.install "yoga.wasm"
-        (bin/"tdx").write <<~EOS
-          #!/bin/bash
-          cd "#{libexec}" && exec ./tdx "$@"
-        EOS
+        bin.install "tdx-darwin-x64" => "tdx"
       end
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-linux-arm64.zip"
-      sha256 "86fecf8e1a401f4bfeb00cdf64bb9ab202d550679827a08e9522b49d4337d0da"
+      url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-linux-arm64"
+      sha256 "119792880740f0444229b434c29f307aaaf7908ce36548fbc14d11264c1b165c"
 
       def install
-        libexec.install "tdx-linux-arm64" => "tdx"
-        libexec.install "yoga.wasm"
-        (bin/"tdx").write <<~EOS
-          #!/bin/bash
-          cd "#{libexec}" && exec ./tdx "$@"
-        EOS
+        bin.install "tdx-linux-arm64" => "tdx"
       end
     end
 
     on_intel do
-      url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-linux-x64.zip"
-      sha256 "7cf688c4f965efcb91a0a8217c30f686bb8914bd2249eb4327e4438c87164462"
+      url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-linux-x64"
+      sha256 "6bf0a5a1204fc85f40f11580f292d8a15d2712181996ac28ff99d30a674687d5"
 
       def install
-        libexec.install "tdx-linux-x64" => "tdx"
-        libexec.install "yoga.wasm"
-        (bin/"tdx").write <<~EOS
-          #!/bin/bash
-          cd "#{libexec}" && exec ./tdx "$@"
-        EOS
+        bin.install "tdx-linux-x64" => "tdx"
       end
     end
   end
