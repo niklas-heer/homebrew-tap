6f16d872035df57b46c507b018abe9191880092b
Niklas Heer <me@nheer.io>
2025-11-21 22:39:18 +0100

    tdx 0.2.2 fixed version

--- Formula/tdx.rb
+++ Formula/tdx.rb
@@ -7,11 +7,15 @@ class Tdx < Formula
   on_macos do
     on_arm do
       url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-darwin-arm64.zip"
-      sha256 "c2315821147c0d093b2cab72b1db65b45369cbf704d8434480a76a06cda8e728"
+      sha256 "8b8b0ff6a5b8e8e1e9f0a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2"
 
       def install
-        bin.install "tdx-darwin-arm64" => "tdx"
-        bin.install "yoga.wasm"
+        libexec.install "tdx-darwin-arm64" => "tdx"
+        libexec.install "yoga.wasm"
+        (bin/"tdx").write <<~EOS
+          #!/bin/bash
+          cd "#{libexec}" && exec ./tdx "$@"
+        EOS
       end
     end
 
@@ -20,8 +24,12 @@ class Tdx < Formula
       sha256 "61da7721736cc34b4e91be290a2c8d79b24785fa2e48e5e486e5390f23e60518"
 
       def install
-        bin.install "tdx-darwin-x64" => "tdx"
-        bin.install "yoga.wasm"
+        libexec.install "tdx-darwin-x64" => "tdx"
+        libexec.install "yoga.wasm"
+        (bin/"tdx").write <<~EOS
+          #!/bin/bash
+          cd "#{libexec}" && exec ./tdx "$@"
+        EOS
       end
     end
   end
@@ -32,8 +40,12 @@ class Tdx < Formula
       sha256 "86fecf8e1a401f4bfeb00cdf64bb9ab202d550679827a08e9522b49d4337d0da"
 
       def install
-        bin.install "tdx-linux-arm64" => "tdx"
-        bin.install "yoga.wasm"
+        libexec.install "tdx-linux-arm64" => "tdx"
+        libexec.install "yoga.wasm"
+        (bin/"tdx").write <<~EOS
+          #!/bin/bash
+          cd "#{libexec}" && exec ./tdx "$@"
+        EOS
       end
     end
 
@@ -42,8 +54,12 @@ class Tdx < Formula
       sha256 "7cf688c4f965efcb91a0a8217c30f686bb8914bd2249eb4327e4438c87164462"
 
       def install
-        bin.install "tdx-linux-x64" => "tdx"
-        bin.install "yoga.wasm"
+        libexec.install "tdx-linux-x64" => "tdx"
+        libexec.install "yoga.wasm"
+        (bin/"tdx").write <<~EOS
+          #!/bin/bash
+          cd "#{libexec}" && exec ./tdx "$@"
+        EOS
       end
     end
   end
