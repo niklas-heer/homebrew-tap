27bdae5322c90855b7ae4234c4d24aef46f7f46a
Niklas Heer <me@nheer.io>
2026-07-16 19:23:50 +0200

    fix: modernize tdx formula install

--- Formula/tdx.rb
+++ Formula/tdx.rb
@@ -8,19 +8,11 @@ class Tdx < Formula
     on_arm do
       url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-darwin-arm64"
       sha256 "ba965381b0901c64e1769e79e7793f2b68f45795e5dce7cd0df3f32791f9d0cf"
-
-      def install
-        bin.install "tdx-darwin-arm64" => "tdx"
-      end
     end
 
     on_intel do
       url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-darwin-amd64"
       sha256 "fc1edd2acc5e5ebde6e0a5a3df370fecdd4022b5f355a88d383c34128241dfa8"
-
-      def install
-        bin.install "tdx-darwin-amd64" => "tdx"
-      end
     end
   end
 
@@ -28,23 +20,19 @@ class Tdx < Formula
     on_arm do
       url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-linux-arm64"
       sha256 "a3f784b7e93dbaf5117b1929f38fac4b039c7d8804b7f0471954df865ab26ac2"
-
-      def install
-        bin.install "tdx-linux-arm64" => "tdx"
-      end
     end
 
     on_intel do
       url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-linux-amd64"
       sha256 "25e5ee1738f1d48a6feb09d4e2efe4b45088430028c38f749bef69747b4d3308"
-
-      def install
-        bin.install "tdx-linux-amd64" => "tdx"
-      end
     end
   end
 
+  def install
+    bin.install Dir["tdx-*"].first => "tdx"
+  end
+
   test do
-    system "#{bin}/tdx", "--help"
+    system bin/"tdx", "--help"
   end
 end
