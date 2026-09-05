75d04c38af9cbf6b0fb9bf014c37b13fd6ad4fe2
Niklas Heer <me@nheer.io>
2026-09-05 15:34:12 +0200

    chore: update tdx to v0.14.0

--- Formula/tdx.rb
+++ Formula/tdx.rb
@@ -1,30 +1,30 @@
 class Tdx < Formula
   desc "Your todos, in markdown, at terminal speed"
   homepage "https://github.com/niklas-heer/tdx"
-  version "0.13.1"
+  version "0.14.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-darwin-arm64"
-      sha256 "51892a025d4359ad89a22d55867b6e1cc29b9051b62a276cb819ccc82c8e026d"
+      sha256 "55fbfd3d0654ff8b7f8b074ad32c6631ad11df7e8c180f5f11dd39c0456af473"
     end
 
     on_intel do
       url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-darwin-amd64"
-      sha256 "5394704b10d2c6bbb5fedb3b2b25f1567f90368b4d740e3e2f37a00254b92cdf"
+      sha256 "a73360897a3dd478b0ee3e560d7317f4a3060bf116424676d08ac07c51103716"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-linux-arm64"
-      sha256 "678beaf3876c68e247301cc5e76bfd8c527120289636922dedde10c0cacd4f64"
+      sha256 "1ddb7d866014b35f5265d8647a92d18259601e9660915610bab23bf15e8dfa51"
     end
 
     on_intel do
       url "https://github.com/niklas-heer/tdx/releases/download/v#{version}/tdx-linux-amd64"
-      sha256 "e46464dfcb2fd8f6504cd73e10c59de41823e1210e5db1677a83cca2d638c06f"
+      sha256 "a21f4b83d7731b946e4820f8f52e373197efd8cd0eae0e8dbfd905b3e9e16e2a"
     end
   end
 
