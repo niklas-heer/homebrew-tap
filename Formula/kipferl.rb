5c274acee6fd3a055b3ec4522fff6406623fc90c
Niklas Heer <me@nheer.io>
2026-09-05 16:09:44 +0200

    chore: update kipferl to v0.7.0

--- Formula/kipferl.rb
+++ Formula/kipferl.rb
@@ -1,26 +1,26 @@
 class Kipferl < Formula
   desc "Bake Python CLI apps into fast standalone binaries"
   homepage "https://kipferl.dev"
-  version "0.6.0"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/niklas-heer/kipferl/releases/download/v#{version}/kipferl-macos-aarch64"
-      sha256 "f028d1522147a0e1999dec1da0d9983094a00217a4f713c52c6669388810ebba"
+      sha256 "a732d260524e7149fc0da9c151c731b5a7bdb284a7906698d441f7518dae6fbd"
     else
       url "https://github.com/niklas-heer/kipferl/releases/download/v#{version}/kipferl-macos-x86_64"
-      sha256 "4ab006a0802599f2425d82a125ef89f648eeb84630abd2c16a982e530c8e1fa6"
+      sha256 "e29b9e0e3839e79fd323490370b26863720c8ef74bf8af977744fca2aa2c1179"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/niklas-heer/kipferl/releases/download/v#{version}/kipferl-linux-aarch64"
-      sha256 "7556c399c952e4e466f233d3193e3b3665d9793f7141a62ba390d758f74a12ef"
+      sha256 "95c8a20624b2775fd2f9264e1e1c0d6152251b5522f6a4633f569108d9f13c7e"
     else
       url "https://github.com/niklas-heer/kipferl/releases/download/v#{version}/kipferl-linux-x86_64"
-      sha256 "3073191e668a5f051044995148514b422e22a21fcdf7164272be1a9866056343"
+      sha256 "de7672437d65ac449c3aea762069ff2368d308a2f48d63fc2c273f2507abbb7a"
     end
   end
 
