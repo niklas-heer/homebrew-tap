a31fcd8c1fe617d90393a6de59792dc404f6031c
niklas-heer <1914063+niklas-heer@users.noreply.github.com>
2026-09-05 20:39:50 +0000

    chore(quirl): update to v0.2.0

--- Formula/quirl.rb
+++ Formula/quirl.rb
@@ -5,21 +5,21 @@ class Quirl < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/niklas-heer/quirl/releases/download/v0.1.0/quirl-v0.1.0-aarch64-apple-darwin.tar"
-      sha256 "1c9737b3efafe23da4eba1f833b34f692ff549651062a052263405e7e1f8951e"
+      url "https://github.com/niklas-heer/quirl/releases/download/v0.2.0/quirl-v0.2.0-aarch64-apple-darwin.tar"
+      sha256 "d843364ef13c80deed3b9b25c92340dd751997d033f9e620638519e6d36ea9ae"
     else
-      url "https://github.com/niklas-heer/quirl/releases/download/v0.1.0/quirl-v0.1.0-x86_64-apple-darwin.tar"
-      sha256 "dfd96b405eeebceedf6c050cbb71b33b878cfdcd600a38fbe262a3a9538e0a54"
+      url "https://github.com/niklas-heer/quirl/releases/download/v0.2.0/quirl-v0.2.0-x86_64-apple-darwin.tar"
+      sha256 "0c2ab2d4f4ece3a4c5f946bcdaebd8e93f9973a9ccc058cc42c5dfa0f1411e9f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/niklas-heer/quirl/releases/download/v0.1.0/quirl-v0.1.0-aarch64-unknown-linux-gnu.tar"
-      sha256 "d2725da8c92325009fcec93570ab3563851dee9a3508fc8ea344a034e7bb3c8e"
+      url "https://github.com/niklas-heer/quirl/releases/download/v0.2.0/quirl-v0.2.0-aarch64-unknown-linux-gnu.tar"
+      sha256 "77c8ac4b41fed8b6b1ae2e195c6c7add06a7a463482bf75b050a40b0c142b2f2"
     else
-      url "https://github.com/niklas-heer/quirl/releases/download/v0.1.0/quirl-v0.1.0-x86_64-unknown-linux-gnu.tar"
-      sha256 "5f30bea27c7d25e766de90471503397c31c61806d327cc974f6b2d0d9e2fa0f2"
+      url "https://github.com/niklas-heer/quirl/releases/download/v0.2.0/quirl-v0.2.0-x86_64-unknown-linux-gnu.tar"
+      sha256 "c7b641943960d1e702ec26f60baccd7b26048acf491553e9708c0dc96d1cd4af"
     end
   end
 
@@ -29,7 +29,7 @@ class Quirl < Formula
   end
 
   test do
-    assert_match "quirl 0.1.0", shell_output("#{bin}/quirl --version")
+    assert_match "quirl 0.2.0", shell_output("#{bin}/quirl --version")
     %w[LICENSE THIRD_PARTY_NOTICES.md THIRD_PARTY_LICENSES.txt].each do |notice|
       assert_path_exists pkgshare/"licenses"/notice
     end
